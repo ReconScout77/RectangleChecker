204643dc385601d4b70d1e8bd3b585a5c3dcdd8c
Parul Mishra Dubedy and Robert Murray <parul@>
2017-08-02 08:52:39 -0700

    this is the first commit which added the logic of accepting length and width from the user and processing the info to calculate square and cube

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using RectangleChecker.Models;
+
+namespace RectangleChecker.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet("/rectangle_result")]
+        public ActionResult Result()
+        {
+            Dictionary<string, object> Shapes = new Dictionary<string, object>();
+            Rectangle myRectangle = new Rectangle(Int32.Parse(Request.Query["side-length"]), Int32.Parse(Request.Query["side-width"]));
+            Shapes.Add("ResultingRectangle", myRectangle);
+            if (myRectangle.IsSquare())
+            {
+                Cube myCube = new Cube(myRectangle);
+                Shapes.Add("ResultingCube", myCube);
+            }
+            return View("RectangleResult", Shapes);
+        }
+
+    }
+}
